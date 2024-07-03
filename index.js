f63c6a30569060d33ea3d7254ad40c8145dc3ae9
wanjala <ranjala90@gmail.com>
2024-07-03 15:33:28 +0300

    functions code

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Follow along with the examples here
+// // Follow along with the examples here
+// function greet () {
+//   console.log("Hello, Greatstack")
+// }
+// greet();
+
+// function sayHello () {
+//   console.log("Hello, World");
+// }
+// sayHello();
+
+// function sayHelloToGuadalupe () {
+//   console.log("Hello, Guadalupe")
+
+// }
+// sayHelloToGuadalupe(); //Calling function
+
+// function welcomeNana () {
+//   console.log("Welcome, My love");
+// }
+// welcomeNana(); //Calling function
+
+// function greet(firstName, lastName) {
+//   console.log("Hello " + firstName + " " + lastName);
+
+// }
+// greet("Nancie", "Njenga");
+
+
+// Anonimous functions
+// let num = function(x,y) {
+//   return x+y;
+// }
+// console.log(sum (10,30));
+
+
+// Anonimous functions
+
+(
+function(){
+  console.log("Welcome Home");
+}
+)
